f0ac5663f56747b9440e1dbb492644d02b4203e8
PedroCardoso2 <www.pedrolucascardoso@gmail.com>
2023-10-24 17:38:39 -0300

    Inputs modification and button hover

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,6 +3,8 @@ import style from "@/Home/home.module.css";
 import { Conteiner } from "@/components/ConteinerContact/Contact-component";
 import { Input } from "@/components/Inputs/Input-component";
 import { Button } from "@/components/Button/button-component";
+import { FiPhoneCall } from "react-icons/fi";
+import {AiOutlineUser} from "react-icons/ai";
 
 export const Home = () => {
     const text = "Pedro"
@@ -11,9 +13,11 @@ export const Home = () => {
         <NavBar/>
         <Conteiner text={text}>
             <div className={style.login}>
-                <div className={style.user} id={style.op}><Input/></div>
-                <div className={style.passWord} id={style.op}><Input/></div>
-               <div className={style.button} id={style.op}><Button name="ENVIAR"/></div>  
+                <div className={style.user} id={style.op}><AiOutlineUser/><Input placeholder=" Nome Completo"/></div>
+
+                
+                <div className={style.passWord} id={style.op}><FiPhoneCall/><Input placeholder=" Número"/></div>
+               <div className={style.button} ><Button name="ENVIAR"/></div>  
             </div>
             
             
--- src/components/Button/ButtonProps.ts
+++ src/components/Button/ButtonProps.ts
@@ -1,3 +1,4 @@
 export type ButtonProps = {
     name: string
+    className: string
 }
